fb6a8ebd9772d925f6d85668b3635c35214bae1a
DESKTOP-5PF6AFO\IT <vechet240@gmail.com>
2025-08-15 23:03:41 +0700

    seed data for user role

--- Ledgerly.API/Data/LedgerlyAuthDbContext.cs
+++ Ledgerly.API/Data/LedgerlyAuthDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Ledgerly.API.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,5 +8,17 @@ namespace Udemy.Data
     public class LedgerlyAuthDbContext : IdentityDbContext
     {
         public LedgerlyAuthDbContext(DbContextOptions<LedgerlyAuthDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole { Name = "System Admin", NormalizedName = "ROLE_SYSTEM_ADMIN" },
+                new IdentityRole { Name = "Admin", NormalizedName = "ROLE_ADMIN" },
+                new IdentityRole { Name = "User", NormalizedName = "ROLE_USER" }
+            );
+        }
+
     }
 }
