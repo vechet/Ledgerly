7b8533190e29d11f35e391c09f4556adcf1e8e7d
DESKTOP-5PF6AFO\IT <vechet240@gmail.com>
2025-08-15 23:02:54 +0700

    seed data for user role

---
+++ Ledgerly.API/Migrations/LedgerlyAuthDb/20250815133929_initRoles.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Ledgerly.API.Migrations.LedgerlyAuthDb
+{
+    /// <inheritdoc />
+    public partial class initRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "4a7e160e-fa71-4b01-bdf0-15ef72e7f25f", null, "Admin", "ROLE_ADMIN" },
+                    { "c4372eec-9e53-434b-afb6-81083afcd22f", null, "User", "ROLE_USER" },
+                    { "fb0890ca-406e-4bf7-ac9e-d4bf979879be", null, "System Admin", "ROLE_SYSTEM_ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4a7e160e-fa71-4b01-bdf0-15ef72e7f25f");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c4372eec-9e53-434b-afb6-81083afcd22f");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "fb0890ca-406e-4bf7-ac9e-d4bf979879be");
+        }
+    }
+}
